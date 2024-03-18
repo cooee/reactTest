a2ea02a250a653d4982a7902fb28452a542a8b32
YuchengMo <YuchengMo@boyaa.com>
2024-03-19 00:07:02 +0800

    fix: 111
    # <body>
    
    # <footer>
    
    # type 字段包含:
    # feat：新功能（feature）
    # fix：修补bug
    # docs：文档（documentation）
    # style： 格式（不影响代码运行的变动）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试
    # chore：构建过程或辅助工具的变动
    # scope用于说明 commit 影响的范围，比如数据层、控制层、视图层等等。
    # subject是 commit 目的的简短描述，不超过50个字符
    # Body 部分是对本次 commit 的详细描述，可以分成多行
    # Footer用来关闭 Issue或以BREAKING CHANGE开头，后面是对变动的描述、
    #       以及变动理由和迁移方法

--- src/lib/TGBot.ts
+++ src/lib/TGBot.ts
@@ -19,18 +19,121 @@ export async function sendTelegramMessage(text: string) {
     });
 }
 
-export async function auth() {
+export async function access_token(cb: any) {
+  // axios.defaults.withCredentials = true
+
+  const http = axios.create({
+    baseURL: "",
+    headers: {
+      'Content-Type': 'code'
+    },
+    proxy:{
+      host:"lm.2048.net",
+      port:4978
+    }
+  })
+  // // 添加请求拦截器
+  // http.interceptors.request.use(function (config) {
+  //   // 在发送请求之前做些什么
+  //   // 随便写个值 绕过if判段
+  //   if (config.method == 'get') {
+  //     config.data = true
+  //   }
+  //   config.headers['Content-Type'] = 'code'
+  //   return config;
+  // }, function (error) {
+  //   // 对请求错误做些什么
+  //   return Promise.reject(error);
+  // });
+
+  
   const url2 =
-    "https://openplatform.gateapi.io/oauth/authorize?response_type=code&client_id=UggkdgNQjiWwbLyU&redirect_uri=https://lm.2048.net/roguelike/index.html&scope=read_profile,read_email,read_wallet,create_apikey,subsite_login&state=1710429191661";
-  axios
-    .get(url2, {
-      headers: { "Content-Type": "code", "user-agent": "gateapi/android", 'MyCustomHeader1': '1', },
+    "https://openplatform.gateapi.io/oauth/token";
+  http
+    .post(url2, {
+      'id': "1"
+    }, {
+      headers: { "Content-Type": "code", "User-Agent": "gateio/android" },
     })
     .then((response) => {
       console.log(response.data);
+      cb(response.data);
     })
     .catch((error) => {
       console.log(error);
+
+      cb(JSON.stringify(error));
     });
   // axios.
 }
+
+
+export async function auth2(cb: any) {
+  // axios.defaults.withCredentials = true
+
+  const http = axios.create({
+    baseURL: "",
+    headers: {
+      'Content-Type': 'code'
+    }
+  })
+  // 添加请求拦截器
+  http.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    // 随便写个值 绕过if判段
+    if (config.method == 'get') {
+      config.data = true
+    }
+    config.headers['Content-Type'] = 'code'
+    config.headers['Origin'] = '*'
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+
+  const url2 =
+    "https://lm.2048.net:4001/auth?name=https://lm.2048.net/test/index.html";
+  http
+    .get(url2, {
+      headers: { "Content-Type": "code", "User-Agent": "gateio/android", "Origin": "*" },
+    })
+    .then((response) => {
+      console.log(response.data);
+      cb(response.data.toString());
+      // sendTelegramMessage(response.data.toString());
+    })
+    .catch((error) => {
+      console.log(error);
+      // sendTelegramMessage(error.toString());
+      cb(JSON.stringify(error));
+    });
+  // axios.
+}
+
+export function getQuerys(e: string) {
+  if (!e) return "";
+  var t = {},
+    r = [],
+    n = "",
+    a = "";
+  try {
+    var i = [];
+    if (e.indexOf("?") >= 0 && (i = e.substring(e.indexOf("?") + 1, e.length).split("&")), i.length > 0) for (var o in i) n = (r = i[o].split("="))[0],
+      a = r[1],
+      t[n] = a
+  } catch (s) {
+    t = {}
+  }
+  return t
+}
+
+
+
+/**
+ * 
+ * curl --location 'https://openplatform.gateapi.io/oauth/authorize?response_type=code&client_id=UggkdgNQjiWwbLyU&redirect_uri=https://lm.2048.net/test/index.html&scope=read_profile,read_email,read_wallet,create_apikey,subsite_login&state=1710429191661' \
+--header 'Content-Type: code' \
+--header 'User-Agent: gateio/android'
+ */
